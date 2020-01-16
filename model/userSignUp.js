6cc1b966984e83d3ff72e44d23601ba7338a9881
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-16 18:43:05 +0530

    query for user login

--- model/userSignUp.js
+++ model/userSignUp.js
@@ -2,5 +2,9 @@ const knex = require("../conection.js")
 let userData = (userDetails)=>{
     return knex.select("*").from("register").insert(userDetails)
 }
+let login=()=>{
+    // return knex.select("*").from("register")
+    return knex.select("*").from("register")
+}
 
-module.exports={userData}
+module.exports={userData,login}
