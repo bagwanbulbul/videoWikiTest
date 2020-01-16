127d40557a2cc21a47f153b45647074deb40e555
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-16 18:44:16 +0530

    created route for user login

--- routes/signUp.js
+++ routes/signUp.js
@@ -1,21 +1,40 @@
 const express = require('express');
-var signUp = express.Router();
-signUp.use(express.json())
+var user = express.Router();
+user.use(express.json())
 const add = require("../model/userSignUp");
 const jwt = require('jsonwebtoken')
 
-signUp.post("/signUp",function(req,res){
+user.post("/signUp",function(req,res){
     let userDetails={name:req.body.name,
         email_id:req.body.email_id,
         password:req.body.password
     }
     let response = add.userData(userDetails)
     response.then((data)=>{
-        let token = jwt.sign({"user":data},"bulbul")
-        res.cookie(token)
-        res.json({token})
+        res.send(data)
     }).catch((err)=>{
         res.send(err)
     })
 })
-module.exports = signUp
+user.get("/login",function(req,res){
+    let email_id=req.body.email_id
+    let password=req.body.password
+    let response=add.login()
+    response.then((data)=>{
+        for(var i=0; i<data.length; i++){
+            if(data[i]["email_id"]==email_id && data[i]["password"]==password){
+                let token = jwt.sign({"user":data},"bulbul")
+                res.cookie(token)
+                res.json({token})
+            }
+            else{
+                res.send("incorect password")
+            }
+        }
+    }).catch((err)=>{
+        res.send(err)
+    })
+})
+
+
+module.exports = user
