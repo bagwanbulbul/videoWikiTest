635b80a613e65e7534b8bc6767bfa0b328384031
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-18 17:55:26 +0530

    havinIn query

--- model/userSignUp.js
+++ model/userSignUp.js
@@ -2,9 +2,10 @@ const knex = require("../conection.js")
 let userData = (userDetails)=>{
     return knex.select("*").from("register").insert(userDetails)
 }
-let login=()=>{
+let login=(email_id)=>{
     // return knex.select("*").from("register")
-    return knex.select("*").from("register")
+    return knex.select('*').from('register').havingIn('email_id',email_id)
+
 }
 
 module.exports={userData,login}
