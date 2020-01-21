dbd933db051912205738d2203e65ab75d509d31a
bagwanbulbul <bulbulbagwan918@gmail.com>
2020-01-21 17:36:20 +0530

    query for insert data in likes table

--- model/userSignUp.js
+++ model/userSignUp.js
@@ -6,5 +6,12 @@ let login=(email_id)=>{
     return knex.select('*').from('register').havingIn('email_id',email_id)
 
 }
+let userPostData = (userPost)=>{
+    return knex.select("*").from("creat_post").insert(userPost)
+}
 
-module.exports={userData,login}
+let postLikes=(likes)=>{
+    return knex.select("*").from("post_likes").insert(likes)
+}
+
+module.exports={userData,login,userPostData,postLikes}
